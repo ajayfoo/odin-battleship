c4dbe6d55ccf3006b615028d6e5e6571c94396b1
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-05-05 21:09:40 +0530

    feat: add dummy click event listener for machine's cell

--- src/view/gameboard/index.js
+++ src/view/gameboard/index.js
@@ -1,32 +1,42 @@
 import { ShipSegmentType } from '../../models/gameboard';
 import './style.css';
 
-const createCell = (shipInfo = null) => {
+const addShipSegmentStyleClass = (view, shipInfo) => {
+  if (shipInfo.type !== ShipSegmentType.STAND_ALONE) {
+    view.classList.add(shipInfo.isVertical ? 'vertical' : 'horizontal');
+  }
+  switch (shipInfo.type) {
+    case ShipSegmentType.HEAD: {
+      view.classList.add('head');
+      break;
+    }
+    case ShipSegmentType.TAIL: {
+      view.classList.add('tail');
+      break;
+    }
+    case ShipSegmentType.MIDDLE: {
+      view.classList.add('middle');
+      break;
+    }
+    default: {
+      view.classList.add('stand-alone');
+      break;
+    }
+  }
+};
+const createCell = (shipInfo, gameboard, forMachine, coordinates) => {
   const view = document.createElement('div');
   view.classList.add('cell');
-  if (shipInfo) {
+  if (shipInfo !== null) {
     view.classList.add('ship');
-    if (shipInfo.type !== ShipSegmentType.STAND_ALONE) {
-      view.classList.add(shipInfo.isVertical ? 'vertical' : 'horizontal');
-    }
-    switch (shipInfo.type) {
-      case ShipSegmentType.HEAD: {
-        view.classList.add('head');
-        break;
-      }
-      case ShipSegmentType.TAIL: {
-        view.classList.add('tail');
-        break;
-      }
-      case ShipSegmentType.MIDDLE: {
-        view.classList.add('middle');
-        break;
-      }
-      default: {
-        view.classList.add('stand-alone');
-        break;
-      }
-    }
+    addShipSegmentStyleClass(view, shipInfo);
+  }
+  if (forMachine) {
+    view.classList.add('for-machine');
+    view.addEventListener('click', () => {
+      const [x, y] = coordinates;
+      console.log(x, y);
+    });
   }
   return view;
 };
@@ -36,21 +46,19 @@ const createGameboardView = (gameboard, forMachine) => {
 
   const [row, col] = gameboard.getSize();
 
-  if (forMachine) {
-    for (let i = 0; i < row * col; ++i) {
-      view.appendChild(createCell());
-    }
-    return view;
-  }
-
   const grid = gameboard.getGrid();
 
   for (let i = 0; i < row; ++i) {
     for (let j = 0; j < col; ++j) {
-      if (grid[i][j] === null) view.appendChild(createCell());
+      if (forMachine) {
+        view.appendChild(createCell(null, gameboard, forMachine, [i, j]));
+        continue;
+      }
+      if (grid[i][j] === null)
+        view.appendChild(createCell(null, gameboard, forMachine, [i, j]));
       else {
         const shipInfo = grid[i][j][1];
-        view.appendChild(createCell(shipInfo));
+        view.appendChild(createCell(shipInfo, gameboard, forMachine, [i, j]));
       }
     }
   }
