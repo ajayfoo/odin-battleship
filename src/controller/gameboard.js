8360bd749f8d39fe5c3111a92be2bf89c0d36f90
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-05-05 16:28:17 +0530

    style: add ocean-like background colors

--- src/controller/gameboard.js
+++ src/controller/gameboard.js
@@ -3,8 +3,16 @@ import { createGameboard } from '../models/gameboard';
 
 const predeterminedGameboard = () => {
   const gameboard = createGameboard();
-  gameboard.placeShipAt([1, 'A'], true, 2);
-  gameboard.placeShipAt([3, 'C'], false, 3);
+  gameboard.placeShipAt([1, 'A'], false, 2);
+  gameboard.placeShipAt([1, 'D'], true, 3);
+  gameboard.placeShipAt([1, 'G'], false, 4);
+  gameboard.placeShipAt([3, 'A'], true, 4);
+  gameboard.placeShipAt([3, 'F'], false, 3);
+  gameboard.placeShipAt([3, 'J'], true, 2);
+  gameboard.placeShipAt([6, 'F'], true, 2);
+  gameboard.placeShipAt([6, 'J'], true, 3);
+  gameboard.placeShipAt([8, 'A'], false, 2);
+  gameboard.placeShipAt([10, 'F'], false, 5);
   return gameboard;
 };
 const createGameboardController = () => {
