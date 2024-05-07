5ad47c767de40585e85b8a77e4d24a53e9649653
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-05-07 13:28:42 +0530

    feat: make machine player randomly select a cell after player1's turn

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,23 +1,48 @@
 import createView from '../view';
 import { createPlayerController } from './player';
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+const getRandomIndices = () => {
+  const indices = [];
+  for (let i = 0; i < 10; ++i) {
+    for (let j = 0; j < 10; ++j) {
+      indices.push([i, j]);
+    }
+  }
+  shuffleArray(indices);
+  return indices;
+};
+
 const createController = () => {
   const player1Controller = createPlayerController('Player1');
+
+  const gameboard1Model = player1Controller.getGameboardController().getModel();
+  const gameboard1View = player1Controller.getGameboardController().getView();
+
+  const randomIndices = getRandomIndices();
   const player2Controller = createPlayerController('Machine', true);
-  console.log('controller');
   player2Controller
     .getGameboardController()
     .getView()
     .addEventListener('userPlayed', () => {
-      console.log('user has played');
-      const gameboard1Model = player1Controller
-        .getGameboardController()
-        .getModel();
-      const gameboard1View = player1Controller
-        .getGameboardController()
-        .getView();
-      gameboard1Model.receiveAttack([1, 'A']);
-      gameboard1View.style.backgroundColor = 'white';
+      if (randomIndices.length === 0) {
+        alert('Game Over');
+        return;
+      }
+      const [row, col] = randomIndices.pop();
+      gameboard1Model.receiveAttack(
+        gameboard1Model.indicesToCoordinates([row, col]),
+      );
+      const targetCell = gameboard1View.querySelector(
+        `div[data-row="${row}"][data-col="${col}"]`,
+      );
+      targetCell.style.backgroundColor = 'yellow';
     });
 
   const view = createView(
