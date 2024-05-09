07808d0dcc61e853cee51be9ba1109087e95bd60
Ajay K <76083469+ajaypep@users.noreply.github.com>
2024-05-09 16:14:53 +0530

    feat: reset game when clicked on New Game btn

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
 import './style.css';
 import createController from './controllers';
 
-const controller = createController();
+let controller = createController();
 const mainEle = document.querySelector('body>main');
 
 mainEle.appendChild(controller.getView());
+
+window.addEventListener('newGame', () => {
+  const tempController = createController();
+  mainEle.replaceChild(tempController.getView(), controller.getView());
+  controller = tempController;
+});
